2357b7467d09875d14ef33caa5a21b7b0e0a7a60
Mamesoke <mamesoke@gmail.com>
2018-06-29 20:47:27 +0200

    Last version

---
+++ Assets/Scripts/ToggleClickButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleClickButton : MonoBehaviour {
+    public GameObject canvas;
+	
+    public void enableDisableCanvas()
+    {
+        canvas.SetActive(!canvas.activeSelf);
+    }
+
+}
